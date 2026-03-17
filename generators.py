b551d6d70f3ecf4506346a0675956fb94cad0d8d
Piotrek <piotrekewald@gmail.com>
2026-03-17 20:48:48 +0100

    Wszystkie sposob generowania ciagu

--- generators.py
+++ generators.py
@@ -1,14 +1,43 @@
 import random
 
-# ----------------------------- Losowy ciąg liczb ---------------------------- #
+# ----------------------------- Zwraca losowy ciąg liczb ---------------------------- #
 def generate_random(n):
-    return [random.randint(1,10000) for _ in range(n)]
+    wynik = []
+    for i in range(n):
+        random_num = random.randint(1, 10000)
+        wynik.append(random_num)
+    return wynik
 
-# ------------------- Losowy ciąg liczb posortowany rosnąco ------------------ #
+# ------------------- Zwraca losowy ciąg liczb posortowany rosnąco ------------------ #
 def generate_ascending(n):
-    return list(range(1, n+1))
+    wynik = []
+    for i in range(1, n + 1):
+        wynik.append(i)
+    return wynik
 
-# ------------------ Losowy ciąg liczb posortowany malejąco ------------------ #
+# ------------------ Zwraca losowy ciąg liczb posortowany malejąco ------------------ #
 def generate_descending(n):
-    return list(range(n, 0, -1))
+    wynik = []
+    for i in range(n, 0, -1):
+        wynik.append(i)
+    return wynik
 
+# ---------------------------- Zwraca ciąg stały liczby losowej ---------------------------- #
+def generate_constant(n):
+    wynik = []
+    random_num = random.randint(1, 10000)
+    for i in range(n):
+        wynik.append(random)
+    return wynik
+
+# -------------------------- Zwraca ciąg A-kształtny ------------------------- #
+def generate_a_shaped(n):
+    wynik = []
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            wynik.append(i)
+    for i in range(n, 0, -1):
+        if i % 2 == 0:
+            wynik.append(i)
+    
+    return wynik
