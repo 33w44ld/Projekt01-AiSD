007fb237862f0ad02294c8ceca800b963156c873
Marcin <cinekweiss@gmail.com>
2026-03-18 16:54:03 +0100

    insertion i selection

--- sorting.py
+++ sorting.py
@@ -1,1 +1,23 @@
-from generators import generate_random, generate_ascending, generate_descending, generate_constant, generate_a_shaped
+# ------------------------------ Insertion Sort ------------------------------ #
+def insertion_sort(arr):
+    data = arr[:]
+    for i in range(1, len(data)):
+        key = data[i]
+        j = i - 1
+        while j >= 0 and key < data[j]:
+            data[j + 1] = data[j]
+            j -= 1
+        data[j + 1] = key
+    return data
+
+# ------------------------------ Selection Sort ------------------------------ #
+def selection_sort(arr):
+    data = arr[:]
+    n = len(data)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if data[j] < data[min_idx]:
+                min_idx = j
+        data[i], data[min_idx] = data[min_idx], data[i]
+    return data
